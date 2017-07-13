029a5950c5d551f10a6ffd8c86cc99d8e6630f5d
Roberto Aguiar Lima <roberto@iMac-de-Roberto-6.local>
2017-07-12 22:06:07 -0300

    event

--- app/index.jsx
+++ app/index.jsx
@@ -2,19 +2,81 @@ import React from 'react';
 import {render} from 'react-dom';
 import ReactDOM from 'react-dom';
 
+class Profile extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	
+	render () {
+		let hobbies = this.props.hobbies.map(hobbie => {
+			return (<li>{hobbie}</li>)
+		})
+		return (
+			<div>
+				<h2>{this.props.name}</h2>
+				<p>
+					{this.props.name} is {this.props.age} and{this.props.bio}</p>
+				<h3>Hobbies</h3>
+				<ul>
+					{hobbies}
+				</ul>
+			</div>
+		)
+	}
+}
+
 class App extends React.Component {
 	constructor(props) {
 	       super(props)
 	       this.state = {
 		       profiles: [
-			       {},
-			       {},
-			       {}
+			       {
+				       name: 'Sue',
+				       age: 30,
+				       bio: 'engoy swimming and biking',
+				       hobbies: ['swimming', 'biking']
+			       },
+			       {
+				       name: 'Bill',
+				       age: 40,
+				       bio: 'enjoys long walks on the beach',
+				       hobbies: ['gardening', 'games']
+			       }
 		       ]
 	       }
 	}	       
+	addProfile(e) {
+		let user = {
+			name:'Joe',
+			age:40,
+			bio:'like sports',
+			hobbies:['basketball', 'baseball']
+		}
+		this.setState(
+			{
+				profiles: this.state.profiles.concat([user])
+			}
+		)
+	}
+
 	render () {
-		return (<p>Hello</p>)
+		let profiles = this.state.profiles.map(profile=> {
+			return (
+				<Profile 
+					name={profile.name}
+					age= {profile.age}
+					bio= {profile.bio}
+					hobbies = {profile.hobbies}
+				/>
+			)
+		})
+		return (
+			<div>
+				<h1>Profiles</h1>
+				{profiles}
+				<button onClick={this.addProfile.bind(this)}>Add new profile</button>
+			</div>
+		)
 	}
 }
 
